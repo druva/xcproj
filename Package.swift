9144549a6c0db2de0c00f974366307fbb3cbbd7d
Michael McMahon <mmcmahon@mobitv.com>
2019-04-01 20:23:55 -0700

    Swift 5 test

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
         ],
     dependencies: [
         .package(url: "https://github.com/kylef/PathKit.git", .upToNextMajor(from: "0.9.1")),
-        .package(url: "https://github.com/tadija/AEXML.git", .upToNextMajor(from: "4.3.0")),
+        .package(url: "https://github.com/tadija/AEXML.git", .upToNextMajor(from: "4.4.0")),
         .package(url: "https://github.com/JohnSundell/ShellOut.git", from: "2.0.0")
         ],
     targets: [
